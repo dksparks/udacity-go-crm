759bcbc075d97cec7b49683d28bad8dec2d12138
Doug Sparks <dksparks@gmail.com>
2024-07-20 18:18:03 -0400

    add functionality to get all customers

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "github.com/gorilla/mux"
+)
+
+type Customer struct {
+  Name string `json:"name"`
+  // note that id is a string, not an int
+  Id string `json:"id"`
+  Adult bool `json:"adult"`
+}
+
+var database = []Customer{
+  Customer{"Alan Grant", "0230", true},
+  Customer{"Ellie Sattler", "4905", true},
+  Customer{"Ian Malcolm", "3440", true},
+  Customer{"Donald Gennaro", "8699", true},
+  Customer{"Lex Murphy", "4000", false},
+  Customer{"Tim Murphy", "7308", false},
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusOK)
+  json.NewEncoder(w).Encode(database)
+}
+
+func main() {
+  router := mux.NewRouter()
+  router.HandleFunc("/customers", getAllCustomers).Methods("GET")
+  port := "3000"
+  fmt.Println("Server running on port", port)
+  http.ListenAndServe(":" + port, router)
+}
