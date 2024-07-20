f0afeb9a5b53cc9df8ad5bfa8067725bbe7874e4
Doug Sparks <dksparks@gmail.com>
2024-07-20 18:54:51 -0400

    add functionality to get a single customer

--- main.go
+++ main.go
@@ -1,38 +1,48 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "encoding/json"
-  "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type Customer struct {
-  Name string `json:"name"`
-  // note that id is a string, not an int
-  Id string `json:"id"`
-  Adult bool `json:"adult"`
+	Name  string `json:"name"`
+	Adult bool   `json:"adult"`
 }
 
-var database = []Customer{
-  Customer{"Alan Grant", "0230", true},
-  Customer{"Ellie Sattler", "4905", true},
-  Customer{"Ian Malcolm", "3440", true},
-  Customer{"Donald Gennaro", "8699", true},
-  Customer{"Lex Murphy", "4000", false},
-  Customer{"Tim Murphy", "7308", false},
+var database = map[string]Customer{
+	"0230": Customer{"Alan Grant", true},
+	"4905": Customer{"Ellie Sattler", true},
+	"3440": Customer{"Ian Malcolm", true},
+	"8699": Customer{"Donald Gennaro", true},
+	"4000": Customer{"Lex Murphy", false},
+	"7308": Customer{"Tim Murphy", false},
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(database)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(database)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := mux.Vars(r)["id"]
+	if customer, ok := database[id]; ok {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(customer)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func main() {
-  router := mux.NewRouter()
-  router.HandleFunc("/customers", getAllCustomers).Methods("GET")
-  port := "3000"
-  fmt.Println("Server running on port", port)
-  http.ListenAndServe(":" + port, router)
+	router := mux.NewRouter()
+	router.HandleFunc("/customers", getAllCustomers).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	port := "3000"
+	fmt.Println("Server running on port", port)
+	http.ListenAndServe(":"+port, router)
 }
