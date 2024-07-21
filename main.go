ebbdc2a3f6443cf68af19de06ba979f0a1220cef
Doug Sparks <dksparks@gmail.com>
2024-07-20 21:17:08 -0400

    add functionality to add a customer

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	// "io"
+	"math/rand"
 	"net/http"
 )
 
@@ -12,13 +14,15 @@ type Customer struct {
 	Adult bool   `json:"adult"`
 }
 
+// The customer database uses nine-digit ids as keys for simplicity.
+// In reality, we would probably do something more sophisticated.
 var database = map[string]Customer{
-	"0230": Customer{"Alan Grant", true},
-	"4905": Customer{"Ellie Sattler", true},
-	"3440": Customer{"Ian Malcolm", true},
-	"8699": Customer{"Donald Gennaro", true},
-	"4000": Customer{"Lex Murphy", false},
-	"7308": Customer{"Tim Murphy", false},
+	"023004163": Customer{"Alan Grant", true},
+	"490520604": Customer{"Ellie Sattler", true},
+	"344093830": Customer{"Ian Malcolm", true},
+	"869930202": Customer{"Donald Gennaro", true},
+	"400025134": Customer{"Lex Murphy", false},
+	"730856990": Customer{"Tim Murphy", false},
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
@@ -38,10 +42,46 @@ func getCustomer(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var customer Customer
+	err := json.NewDecoder(r.Body).Decode(&customer)
+    fmt.Println(customer)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		// Generate a new database id for the customer,
+		// and keep trying until there is no conflict or
+		// until we have tried and failed 1000 times.
+		// This simple approach is for example purposes.
+		// In reality, we would probably do something
+        // more sophisticated.
+		random := rand.New(rand.NewSource(0))
+		failureLimit := int(1e3)
+		failureCount := 0
+		for failureCount < failureLimit {
+			idNum := random.Intn(int(1e9))
+			id := fmt.Sprintf("%09d", idNum)
+			_, conflict := database[id]
+			if !conflict {
+				database[id] = customer
+				w.WriteHeader(http.StatusCreated)
+				break
+			}
+			failureCount++
+		}
+		if failureCount == failureLimit {
+			w.WriteHeader(http.StatusConflict)
+		}
+		json.NewEncoder(w).Encode(database)
+	}
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/customers", getAllCustomers).Methods("GET")
 	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
 	port := "3000"
 	fmt.Println("Server running on port", port)
 	http.ListenAndServe(":"+port, router)
